f31beb0b17106b7387c363e2306643194712cbe1
muhammad-waseem-netixsol <muhammad_waseem_intern@netixsol.com>
2024-01-15 23:14:47 +0500

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@ import mongoose, {model, Schema, models} from "mongoose";
 
 const ProductSchema = new Schema({
   title: {type:String, required:true},
-  description: String,
+  description: {type:String},
   price: {type: Number, required: true},
   images: [{type:String}],
   category: {type:mongoose.Types.ObjectId, ref:'Category'},
